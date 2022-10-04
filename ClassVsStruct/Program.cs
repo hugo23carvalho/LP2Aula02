00b44cfe4d3762312ad05fb814fca712fc7288e1
hugo23carvalho <45176065+hugo23carvalho@users.noreply.github.com>
2022-10-04 10:39:16 +0100

    Update

--- ClassVsStruct/Program.cs
+++ ClassVsStruct/Program.cs
@@ -7,11 +7,19 @@ namespace ClassVsStruct
         public static void Main(string[] args)
         {
 
-            Player pl = new Player{} (p1.Health = 60, p1.Armor = 40); //Ponto 2 do exercicio
-            Player pl = new Player{} (p2.Health = 100, p2.Armor = 100); 
+            Player p1 = new Player{} (p1.Health = 60, p1.Armor = 40); //Ponto 2 do exercicio
+            Player p2 = p1;
+
+            DupHealth(ref p1);
 
             Console.WriteLine($"Player 1 -> Health = {p1.Health}, {p1.Armor}");
             Console.WriteLine($"Player 2 -> Health = {p2.Health}, {p2.Armor}");
+        }
+
+        private static void DupHealth (ref Player player) //ref altera os valores, anteriormente não pq player é struct (cópia) tipo de valor
+        {
+
+            player.Health *= 2;
         }
     }
 }
